ff695c88e693a85aae11da703105b58fa504b989
khalid14414 <qwekukhalid266@gmail.com>
2024-09-16 15:44:39 +0000

    first day

---
+++ scripts/register.js
@@ -0,0 +1,7 @@
+// alert('javaScript works')
+// select from by name
+const registerForm = document.getElementById('register-form');
+// Handle form  submit event
+registerForm.onsubmit=function () {
+    
+}
